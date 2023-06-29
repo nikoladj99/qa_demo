4fc5e908da176c138cf01cc6584efb7dcee74203
nikoladj99 <nikoladj@elfak.rs>
2023-06-29 23:49:36 +0200

    Dodat izbor modela

--- demo_multiple_models.py
+++ demo_multiple_models.py
@@ -1,8 +1,7 @@
-from transformers import TFAutoModelForQuestionAnswering, AutoTokenizer, TFAutoModelForSeq2SeqLM,  T5Tokenizer
+from transformers import TFAutoModelForQuestionAnswering, AutoTokenizer, TFAutoModelForSeq2SeqLM,  T5Tokenizer,TFT5ForConditionalGeneration
 import tensorflow as tf
 import streamlit as st
 import PyPDF2 as pypdf
-import time
 
 @st.cache_data
 def read_pdf(file):
@@ -14,7 +13,27 @@ def read_pdf(file):
         text += page.extract_text()
     return text
 
-def model_func(question,context):
+def t5_model_func(question,context):
+
+    # koristimo pretrenirani model, jos uvek nije fine-tunovan
+    model_name = "google/flan-t5-base"
+    model = TFT5ForConditionalGeneration.from_pretrained(model_name)
+    #model = TFAutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+    # ucitavanje tokenizatora
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # tokenizacija ulaza 
+    input_text = question + " " + context
+    #inputs = tokenizer(input_text,return_tensors="tf")
+
+    input_ids = tokenizer.encode(input_text, return_tensors='tf')
+    outputs = model.generate(input_ids)
+    answer = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    st.write(answer)
+
+def roberta_model_func(question,context):
 
     # koristimo pretrenirani model, jos uvek nije fine-tunovan
     model_name = "deepset/roberta-base-squad2"
@@ -42,15 +61,27 @@ def model_func(question,context):
     )
     st.write(answer)
 
+models = {
+    'T5': t5_model_func,
+    'Roberta': roberta_model_func
+}
+
 def main():
 
     st.title('Sova demo app')
+
+    selected_model = st.selectbox(
+    'Which model would you like to use?',
+    ('T5', 'Roberta'))
+    st.write('You selected:', selected_model)
+    model_func = models[selected_model]
+
     uploaded_file = st.file_uploader("Choose a file", type=["txt","pdf"])
     user_input = st.text_input("Enter your question", "")
     
     if uploaded_file is not None and st.button("run model"):
         text = read_pdf(uploaded_file)
-        model_func(user_input,text)
+        model_func(user_input, text)
     else:
         st.text = "Please choose a file to load"
 
